392a2120247a71ab44c2ea918b5c326c2902d8e8
Balamathias <balamathias05@gmail.com>
2024-09-18 08:58:21 +0100

    Clean up again

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next'
 import React from 'react'
 
 export const metadata: Metadata = {
-  title: "Verify Your Email | Culinara"
+  title: "Login | Culinara"
 }
 
 const Page = () => {
