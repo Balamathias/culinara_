c61209fc4ea43a56479204f0a7062fcf8371209e
Balamathias <balamathias05@gmail.com>
2024-09-18 22:03:50 +0100

    New commit - custom special files

--- src/app/(root)/favorites/page.tsx
+++ src/app/(root)/favorites/page.tsx
@@ -1,9 +1,7 @@
 import Favorites from "@/components/favorites/favorites";
-import PostsSkeleton from "@/components/skeletons/posts";
 import { getUser } from "@/services/auth";
 import { LucideHeart } from "lucide-react";
 import { Metadata } from "next";
-import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: 'Favorite Recipes | Culinara'
@@ -13,28 +11,12 @@ export default async function Page() {
   const user = await getUser()
   return (
     <div className="flex md:p-8 py-6 mx-auto gap-y-3 max-w-7xl w-full gap-x-10">
-      <div className="flex flex-col gap-y-4 md:basis-2/3">
+      <div className="flex flex-col gap-y-4">
         <h2 className="text-2xl md:text-4xl font-semibold py-4 text-muted-foreground flex items-center gap-x-2">
           <LucideHeart size={32} className="mt-1" />
           <span>Your Favorites</span>
         </h2>
-        <Suspense fallback={<PostsSkeleton />}>
-          <Favorites user={user!} />
-        </Suspense>
-      </div>
-
-      <div className="hidden md:flex flex-col gap-y-3">
-        <h2 className="text-xl md:text-2xl font-normal py-4 text-muted-foreground tracking-tighter">Options</h2>
-        
-        <div className="flex flex-col gap-y-2">
-          <div className="rounded-lg py-2.5 bg-primary/15 text-primary px-4">
-            <h3 className="text-lg font-semibold">Sort By</h3>
-            <div className="flex flex-col gap-y-2 mt-2">
-              <button className="text-base font-medium">Most Recent</button>
-              <button className="text-base font-medium">Most Liked</button>
-            </div>
-          </div>
-        </div>
+        <Favorites user={user!} />
       </div>
     </div>
   );
