19f7fdfaf2c27b8390c4bf14dc969fa3886c8b18
Balamathias <balamathias05@gmail.com>
2024-09-19 07:16:59 +0100

    New commit - custom special files

--- src/components/skeletons/profile-thumb.tsx
+++ src/components/skeletons/profile-thumb.tsx
@@ -4,12 +4,12 @@ import { Skeleton } from '../ui/skeleton'
 const ProfileThumbSkeleton = () => {
   return (
     <div className='flex gap-y-4 py-4 flex-col'>
-      <Skeleton className='rounded-xl w-full h-48 relative'>
+      <Skeleton className='rounded-xl w-full h-48 relative bg-primary'>
         <div className='flex absolute left-0 -bottom-1/4'>
           <Skeleton className='w-24 h-24 rounded-full -z-10' />
         </div>
       </Skeleton>
-      <div className='flex justify-between gap-y-1 items-center mt-10'>
+      <div className='flex justify-between flex-row gap-y-1 items-center mt-10'>
         <div className='flex flex-col gap-y-1'>
           <Skeleton className='md:w-48 w-36 h-6' />
           <Skeleton className='w-24 h-4' />
